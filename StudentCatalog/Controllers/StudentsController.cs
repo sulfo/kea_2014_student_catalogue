8b94168f2cc45c9d4ee5a01889c79602677903f1
Ruben Nielsen <nielsen.ruben@gmail.com>
2014-10-11 12:32:20 +0200

    adds missing files

---
+++ StudentCatalog/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentCatalog.Models;
+
+namespace StudentCatalog.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public string WannaPlayDad()
+        {
+            return "NO!";
+        }
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            ViewBag.Lucas = "Hi dad";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                //save
+                return View("Thanks");
+            }
+            else
+            {
+                return View(student);
+            }
+
+            
+        }
+
+    }
+}
---
+++ StudentCatalog/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentCatalog.Models
+{
+    public class Student
+    {
+        //C#: Properties start with capital letter
+
+        //Java way of doing things..
+        //private int studentId;
+
+        //public void setStudentId(int studentId)
+        //{
+        //    this.studentId = studentId;
+        //}
+
+        //public int getStudentId()
+        //{
+        //    return studentId;
+        //}
+
+        public int StudentId {get ; set; }
+
+        [Required(ErrorMessage = "Firstname is required")]
+        public String Firstname { get; set; }
+        
+        [Required(ErrorMessage = "Lastname is required")]
+        public String Lastname { get; set; }
+        
+        [Required(ErrorMessage = "Stupid you!")]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        public String MobilePhone { get; set; }
+
+    }
+}
